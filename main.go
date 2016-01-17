215e5d9ad7678ca95581ef3c4c587ceed00eb657
Maksym Shcheglov <maxscheglov@gmail.com>
2016-01-17 17:18:25 +0100

    Updated the html parsing, removed leftovers.

--- main.go
+++ main.go
@@ -2,53 +2,23 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"io"
 
 	"net/url"
 
-	"strconv"
+	"fmt"
+
+	"path"
 
 	"golang.org/x/net/html"
 )
 
-type Ad struct {
-	Desc  string
-	Price string
-}
-
 func perror(err error) {
 	if err != nil {
-		print("Error!!!")
 		panic(err)
 	}
-}
-
-func getAd(tokenizer *html.Tokenizer) (ok bool, ad Ad) {
-	t := tokenizer.Token()
-
-	isDiv := t.Data == "div"
-	if !isDiv {
-		return
-	}
-
-	for _, a := range t.Attr {
-		if a.Key == "class" && a.Val == "ad-list-title" {
-			tokenizer.Next()
-			title := tokenizer.Token().Data
-			tokenizer.Next()
-			tokenizer.Next()
-			tokenizer.Next()
-			tokenizer.Next()
-			isSold := tokenizer.Token().Data == "Sold"
-			if !isSold {
-				ok = true
-				ad = Ad{Desc: title}
-				return
-			}
-		}
-	}
-	return
 }
 
 func main() {
@@ -66,72 +36,101 @@ func main() {
 }
 
 type Ticket struct {
-	Title string
-	Price float64
-	User  string
-	Url   *url.URL
+	Title    string
+	Qty      uint8
+	Price    float64
+	Currency string
+	User     string
+	Url      *url.URL
+}
+
+func (t *Ticket) String() string {
+	return fmt.Sprintf("%s, %d x %.2f %s by %s, %s", t.Title, t.Qty, t.Price, t.Currency, t.User, t.Url.String())
 }
 
 const (
-	tagSection = "section"
-	tagDiv     = "div"
-	tagH2      = "h2"
-	tagA       = "a"
-	tagArticle = "article"
-	attrProp   = "itemprop"
-	attrClass  = "class"
-	attrHref   = "href"
+	baseUrl     = "https://www.ticketswap.com/"
+	tagSection  = "section"
+	tagDiv      = "div"
+	tagH2       = "h2"
+	tagA        = "a"
+	tagMeta     = "meta"
+	tagArticle  = "article"
+	attrProp    = "itemprop"
+	attrClass   = "class"
+	attrHref    = "href"
+	attrContent = "content"
 
 	adsAttrClassVal = "ad-list"
 	availableData   = "Available"
 
-	ticketAttrPropVal       = "tickets"
-	ticketUrlAttrPropVal    = "offerurl"
-	ticketTitleAttrClassVal = "ad-list-title"
-	ticketPriceAttrClassVal = "ad-list-price"
+	ticketAttrPropVal        = "tickets"
+	ticketUrlAttrPropVal     = "offerurl"
+	ticketUrlQuantityPropVal = "quantity"
+	ticketUrlPricePropVal    = "price"
+	ticketUrlCurrencyPropVal = "currency"
+	ticketUserAttrClassVal   = "name"
+	ticketTitleAttrClassVal  = "ad-list-title"
+	ticketPriceAttrClassVal  = "ad-list-price"
 )
 
 func parseHtml(r io.Reader) []Ticket {
 	tokenizer := html.NewTokenizer(r)
 	var tickets []Ticket
 	var t *Ticket
+	isAvailable := false
 	for {
-		token := tokenizer.Token()
 		tokenType := tokenizer.Next()
+		token := tokenizer.Token()
 		switch tokenType {
 		case html.ErrorToken:
 			// End of the document, we're done
 			return tickets
 		case html.StartTagToken:
-			if token.Data == tagSection { // ads section found
-				tokenizer.Next()
+			if token.Data == tagSection && hasAttr(token.Attr, &html.Attribute{Key: attrClass, Val: adsAttrClassVal}) {
 				// found some ads, check for availability
-
+				tokenizer.Next()
+				tokenizer.Next()
+				tokenizer.Next()
+				isAvailable = tokenizer.Token().Data == availableData
+			}
+			if !isAvailable {
+				continue
 			} else if token.Data == tagArticle && hasAttr(token.Attr, &html.Attribute{Key: attrProp, Val: ticketAttrPropVal}) {
 				t = &Ticket{}
 			} else if token.Data == tagA && hasAttr(token.Attr, &html.Attribute{Key: attrProp, Val: ticketUrlAttrPropVal}) {
-				urlString, _ := getVal(token.Attr, attrHref)
-				url, _ := url.Parse(urlString)
+				urlPath, _ := getVal(token.Attr, attrHref)
+				url, _ := url.Parse(path.Join(path.Base(baseUrl), urlPath))
 				t.Url = url
 			} else if token.Data == tagDiv && hasAttr(token.Attr, &html.Attribute{Key: attrClass, Val: ticketTitleAttrClassVal}) {
 				tokenizer.Next()
 				t.Title = tokenizer.Token().Data
-			} else if token.Data == tagDiv && hasAttr(token.Attr, &html.Attribute{Key: attrClass, Val: ticketPriceAttrClassVal}) {
+			} else if token.Data == tagDiv && hasAttr(token.Attr, &html.Attribute{Key: attrClass, Val: ticketUserAttrClassVal}) {
 				tokenizer.Next()
-				price, _ := strconv.ParseFloat(tokenizer.Token().Data, 64)
-				t.Price = price
+				t.User = tokenizer.Token().Data
 			}
-			// if ok, ad := getAd(tokenizer); ok {
-			// 	fmt.Println(ad.Desc, url)
-			// 	return
-			// }
 		case html.TextToken:
+		case html.SelfClosingTagToken:
+			if !isAvailable {
+				continue
+			} else if token.Data == tagMeta && hasAttr(token.Attr, &html.Attribute{Key: attrProp, Val: ticketUrlPricePropVal}) {
+				priceStr, _ := getVal(token.Attr, attrContent)
+				price, _ := strconv.ParseFloat(priceStr, 64)
+				t.Price = price
+			} else if token.Data == tagMeta && hasAttr(token.Attr, &html.Attribute{Key: attrProp, Val: ticketUrlQuantityPropVal}) {
+				qtyStr, _ := getVal(token.Attr, attrContent)
+				qty, _ := strconv.ParseUint(qtyStr, 10, 8)
+				t.Qty = uint8(qty)
+			} else if token.Data == tagMeta && hasAttr(token.Attr, &html.Attribute{Key: attrProp, Val: ticketUrlCurrencyPropVal}) {
+				currencyStr, _ := getVal(token.Attr, attrContent)
+				t.Currency = currencyStr
+			}
 		case html.EndTagToken:
-			if tokenizer.Token().Data == tagArticle && t != nil {
+			if token.Data == tagArticle && isAvailable && t != nil {
 				tickets = append(tickets, *t)
+				fmt.Println(t.String())
 				t = nil
 			}
-
 		}
 	}
 	return tickets
