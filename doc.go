287120ff1069d6144bb510d044880dcee4048b43
Maksym Shcheglov <maxscheglov@gmail.com>
2016-01-28 21:48:00 +0100

    Removed the doc.go. Added documentation to the ticketswap.go instead.

--- doc.go
+++
@@ -1,4 +0,0 @@
-// Package ticketswap provides a telegram bot api for the https://www.ticketswap.com webservice.
-//
-// The example of bot usage is implemented in cmd/example.go, so check that out.
-package ticketswap
--- ticketswap.go
+++ ticketswap.go
@@ -1,3 +1,6 @@
+// Package ticketswap provides a telegram bot for the https://www.ticketswap.com webservice.
+//
+// The example of bot usage is in cmd/example.go, so check that out.
 package ticketswap
 
 const (
